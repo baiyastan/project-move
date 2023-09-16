ba82a1b97841c1fe72463f6b253c3f7a36e067fd
zarina <akunovazarina1@example.com>
2023-09-15 17:16:55 -0700

    added Footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  return <div>Frontend A2 Okurmen</div>;
+  return (
+    <div>
+      <div>Hi</div>
+    </div>
+  );
 }
 
 export default App;
